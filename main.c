53b7a4903c628f04ee21eb6d663f0d893f76b72c
caua34564 <caua.batalha@escola.pr.gov.br>
2026-03-23 12:22:39 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+quantidade = int(input("Quantos numeros voce deseja digitar? "))
+
+soma = 0
+
+maior = None
+menor = None
+
+for i in range(quantidade):
+    numero = float(input(f"digite o {i+1} numero"))
+    
+    soma += numero
+    if maior is None or numaro > maior:
+        menor = numero
+        
+        menor is None or numero , menor;
+        menor = numero
+         
+media = soma / quantidade
+
+print("\nResultados:")
+print("soma total:", soma)
+print("media:", media)
+print("maior numero:", maior)
+print("menor numero:", menor)
